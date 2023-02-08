5469ab79bb36c662a17336206001b4c73419009f
SantoshNandamuri <santoshknandamuri@gmail.com>
2023-02-08 15:26:59 -0600

    SearchBar component added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const SearchBar = () => {
+  return (
+    <form>
+      <input type="text" placeholder='search'/>
+      <p>
+        <input type="checkbox"/>
+        <span> Show only products in stock</span>
+      </p>
+    </form>
+  );
+}
+
+const FilterableProductTable = props => {
+  const {products} = props;
+  return (
+    <div style={{fontFamily: "sans-serif"}}>
+      <SearchBar />
+      ProductTable
+    </div>
+  );
+}
+
+const PRODUCTS = [
+  {
+    category: "product-category",
+    price: "$25.99",
+    instock: true,
+    name: "product-name"
+  }
+]
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
