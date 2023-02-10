91b66fa477e1886b3794fd83a38728d9f7b93dcd
SantoshNandamuri <santoshknandamuri@gmail.com>
2023-02-09 20:38:04 -0600

    Added ProductTable component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+const ProductRow = props => {
+  const {product} = props;
+
+  return (
+    <tr>
+      <td>{product.name}</td>
+      <td align='right'>{product.price}</td>
+    </tr>
+  );
+}
+
+const ProductTable = props => {
+  const {products} = props;
+  const rows = [];
+
+  products.forEach(product => {
+    rows.push(<ProductRow product={product} key={product.name}/>);
+  });
+
+  return (
+    <table width="100%">
+      <thead>
+        <tr>
+          <th align='left'>Name</th>
+          <th align='right'>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  );
+}
 
 const SearchBar = () => {
   return (
@@ -15,20 +49,33 @@ const SearchBar = () => {
 
 const FilterableProductTable = props => {
   const {products} = props;
+  console.log("productsJson");
   return (
     <div style={{fontFamily: "sans-serif"}}>
       <SearchBar />
-      ProductTable
+      <ProductTable products={products} />
+      
     </div>
   );
 }
 
+// const [data, setdata] = useState([]);
+
+// fetch('https://dummyjson.com/products')
+// .then(res => res.json()).then(json => setdata(json))
+
 const PRODUCTS = [
   {
-    category: "product-category",
+    category: "product-category-1",
     price: "$25.99",
     instock: true,
-    name: "product-name"
+    name: "product-name-1"
+  },
+  {
+    category: "product-category-2",
+    price: "$25.99",
+    instock: true,
+    name: "product-name-2"
   }
 ]
 
